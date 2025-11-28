3512c10989f910a5ed38130913bd3f56d7704288
milanmitreski <milanmitreski@gmail.com>
2025-11-28 10:19:04 +0100

    Third week notes uploaded.

---
+++ materials/v03/vezbe_petak.sql
@@ -0,0 +1,296 @@
+-- 1. Izdvojiti nazive predmeta na koje je IZASAO student sa indeksom 22/2017.
+
+-- Varijanta 1 - resenje spajanjem
+
+SELECT DISTINCT P.NAZIV
+FROM DA.ISPIT I JOIN DA.PREDMET P ON (I.IDPREDMETA = P.ID)
+WHERE I.INDEKS = 20170022
+    AND I.STATUS NOT IN ('p', 'n');
+
+-- Varijanta 2 - podupit sa EXISTS
+
+SELECT P.NAZIV
+FROM DA.PREDMET P
+WHERE EXISTS (
+    SELECT *
+    FROM DA.ISPIT I
+    WHERE I.INDEKS = 20170022
+      AND I.STATUS NOT IN ('p', 'n')
+      AND I.IDPREDMETA = P.ID
+);
+
+-- Varijanta 3 - podupit sa IN
+
+SELECT P.NAZIV
+FROM DA.PREDMET P
+WHERE P.ID IN (
+    SELECT I.IDPREDMETA
+    FROM DA.ISPIT I
+    WHERE I.INDEKS = 20170022
+        AND I.STATUS NOT IN ('p', 'n')
+);
+
+-- Varijanta 4 - podupit as IN, na malo drugaciji nacin
+
+SELECT P.NAZIV
+FROM DA.PREDMET P
+WHERE 20170022 IN (
+    SELECT I.INDEKS
+    FROM DA.ISPIT I
+    WHERE I.IDPREDMETA = P.ID
+        AND I.STATUS NOT IN ('p', 'n')
+);
+
+-- 2. Izdvojiti ime i prezime studenta koji ima ispit polozen sa ocenom 9.
+
+-- Varijanata sa spajanjem
+
+SELECT DISTINCT D.IME, D.PREZIME
+FROM DA.DOSIJE D JOIN DA.ISPIT I ON (D.INDEKS = I.INDEKS)
+WHERE I.STATUS = 'o' AND I.OCENA = 9;
+
+-- Varijanta sa EXISTS
+
+SELECT DISTINCT D.IME, D.PREZIME
+FROM DA.DOSIJE D
+WHERE EXISTS (
+    SELECT *
+    FROM DA.ISPIT I
+    WHERE I.INDEKS = D.INDEKS
+        AND I.OCENA = 9
+        AND I.STATUS = 'o'
+);
+
+-- 3. Izdvojiti indekse studenata koji su polozili bar jedan predmet koji
+-- nije polozio student sa indeksom 22/2017.
+
+SELECT DISTINCT I.INDEKS
+FROM DA.ISPIT I
+WHERE I.OCENA > 5
+    AND I.STATUS = 'o'
+    AND NOT EXISTS (
+        SELECT *
+        FROM DA.ISPIT I1
+        WHERE I1.INDEKS = 20170022
+            AND I1.OCENA > 5
+            AND I1.STATUS = 'o'
+            AND I1.IDPREDMETA = I.IDPREDMETA
+    );
+
+SELECT D.INDEKS
+FROM DA.DOSIJE D
+WHERE EXISTS (
+    SELECT *
+    FROM DA.ISPIT I
+    WHERE I.INDEKS = D.INDEKS
+        AND I.OCENA > 5
+        AND I.STATUS = 'o'
+        AND NOT EXISTS (
+            SELECT *
+            FROM DA.ISPIT I1
+            WHERE I1.IDPREDMETA = I.IDPREDMETA
+                AND I1.OCENA > 5
+                AND I1.STATUS = 'o'
+                AND I1.INDEKS = 20170022
+        )
+);
+
+-- 4. Koriscenjem egzistencijalnog kvantifikatora exists izdvojiti
+-- nazive predmeta koje je polozio student sa indeksom 22/2017.
+
+SELECT P.NAZIV
+FROM DA.PREDMET P
+WHERE EXISTS (
+    SELECT *
+    FROM DA.ISPIT I
+    WHERE I.IDPREDMETA = P.ID
+        AND I.INDEKS = 20170022
+        AND I.OCENA > 5
+        AND I.STATUS = 'o'
+);
+
+-- 5. Izdvojiti naziv predmeta ciji je kurs organizovan u svim skolskim
+-- godinama o kojima postoje podaci u bazi podataka.
+
+SELECT P.NAZIV
+FROM DA.PREDMET P
+WHERE NOT EXISTS (
+    SELECT *
+    FROM DA.SKOLSKAGODINA SG
+    WHERE NOT EXISTS (
+        SELECT *
+        FROM DA.KURS K
+        WHERE K.SKGODINA = SG.SKGODINA
+            AND K.IDPREDMETA = P.ID
+    )
+);
+
+-- Za resenje sa nekorelisanim podupitom pogledati script.sql
+
+-- 6. Izdvojiti podatke o studentu koji je upisao sve skolske godine o
+-- kojima postoje podaci u bazi podataka.
+
+-- Slicna stvar samo sa tabelom DA.UPISGODINE
+
+-- Varijanta sa IN
+
+-- Varijanta sa EXISTS
+
+-- 7. Izdvojiti indekse studenata koji su izasli na neki ispit u svim ispitnim rokovima.
+
+SELECT D.INDEKS
+FROM DA.DOSIJE D
+WHERE NOT EXISTS (
+    SELECT *
+    FROM DA.ISPITNIROK IR
+    WHERE NOT EXISTS (
+        SELECT *
+        FROM DA.ISPIT I
+        WHERE I.INDEKS = D.INDEKS
+            AND I.SKGODINA = IR.SKGODINA
+            AND I.OZNAKAROKA = IR.OZNAKAROKA
+            AND I.STATUS IN ('p', 'n')
+    )
+);
+
+-- Nema takvih studenata!
+
+-- 8. Izdvojiti indekse studenata koji su polagali u svim ispitnim rokovima
+-- odrzanim u 2018/2019. sk. godini.
+
+SELECT D.INDEKS
+FROM DA.DOSIJE D
+WHERE NOT EXISTS (
+    SELECT *
+    FROM DA.ISPITNIROK IR
+    WHERE IR.SKGODINA = 2018
+        AND NOT EXISTS (
+            SELECT *
+            FROM DA.ISPIT I
+            WHERE I.INDEKS = D.INDEKS
+                AND I.SKGODINA = IR.SKGODINA
+                AND I.OZNAKAROKA = IR.OZNAKAROKA
+                AND I.STATUS IN ('p', 'n')
+    )
+);
+
+-- Nema takvih studenata!
+
+-- 9. Izdvojiti podatke o predmetima sa najvecim brojem espb bodova.
+
+-- Varijanta sa EXISTS
+
+SELECT *
+FROM DA.PREDMET P
+WHERE NOT EXISTS (
+    SELECT *
+    FROM DA.PREDMET P1
+    WHERE P1.ESPB > P.ESPB
+);
+
+-- Varijanta sa ALL
+
+SELECT *
+FROM DA.PREDMET P
+WHERE P.ESPB >= ALL (
+    SELECT P1.ESPB
+    FROM DA.PREDMET P1
+);
+
+-- 10. Izdvojiti podatke o studentima sa najranijim datumom diplomiranja.
+
+-- Za vezbu
+
+-- 11. Izdvojiti podatke o svim studentima osim onih sa najranijim datumom diplomiranja.
+
+-- Varijanta sa negacijom od ALL
+
+SELECT *
+FROM DA.DOSIJE D
+WHERE NOT DATDIPLOMIRANJA <= ALL (
+    SELECT D1.DATDIPLOMIRANJA
+    FROM DA.DOSIJE D1
+    WHERE D1.DATDIPLOMIRANJA IS NOT NULL
+) OR DATDIPLOMIRANJA IS NULL;
+
+-- Varijanta sa ANY
+
+SELECT *
+FROM DA.DOSIJE D
+WHERE DATDIPLOMIRANJA > ANY (
+    SELECT D1.DATDIPLOMIRANJA
+    FROM DA.DOSIJE D1
+    WHERE D1.DATDIPLOMIRANJA IS NOT NULL
+) OR DATDIPLOMIRANJA IS NULL;
+
+-- Varijanta sa EXISTS
+
+SELECT *
+FROM DA.DOSIJE D
+WHERE EXISTS (
+    SELECT  *
+    FROM DA.DOSIJE D1
+    WHERE D1.DATDIPLOMIRANJA < D.DATDIPLOMIRANJA
+) OR D.DATDIPLOMIRANJA IS NULL;
+
+-- 12. Izdvojiti podatke o predmetima koje su upisali neki studenti.
+
+-- Varijanta sa SOME
+
+SELECT *
+FROM DA.PREDMET P
+WHERE P.ID = SOME (
+    SELECT UK.IDPREDMETA
+    FROM DA.UPISANKURS UK
+);
+
+-- Varijanta sa ANY
+
+SELECT *
+FROM DA.PREDMET P
+WHERE P.ID = ANY (
+    SELECT UK.IDPREDMETA
+    FROM DA.UPISANKURS UK
+);
+
+-- 13. Za studente koji su polagali ispit u ispitnom roku odrzanom
+-- u 2018/2019. sk. godini izdvojiti podatke o polozenim ispitima.
+-- Izdvojiti indeks, ime, prezime studenta, naziv polozenog predmeta,
+-- oznaku ispitnog roka i skolsku godinu u kojoj je ispit polozen.
+
+-- Prvo izdvajamo podatke o studentima koji su polagali ispit
+-- u ispitnom roku 2018/2019
+
+-- Nakon toga dodajemo informacije o polozenim ispitima
+
+SELECT *
+FROM DA.DOSIJE D JOIN DA.ISPIT I ON (D.INDEKS = I.INDEKS)
+    JOIN DA.PREDMET P ON (I.IDPREDMETA = P.ID)
+WHERE I.STATUS = 'o'
+    AND I.OCENA > 5
+    AND EXISTS (
+    SELECT *
+    FROM DA.ISPIT I
+    WHERE D.INDEKS = I.INDEKS
+        AND I.STATUS NOT IN ('p', 'n')
+        AND I.SKGODINA = 2018
+);
+
+-- 14. Izdvojiti podatke o predmetima koje su polagali svi studenti
+-- iz Berana koji studiraju smer sa oznakom I.
+
+SELECT *
+FROM DA.PREDMET P
+WHERE NOT EXISTS (
+    SELECT *
+    FROM DA.DOSIJE D JOIN DA.STUDIJSKIPROGRAM SP ON (D.IDPROGRAMA = SP.ID)
+    WHERE D.MESTORODJENJA = 'Berane'
+        AND SP.OZNAKA = 'I'
+        AND NOT EXISTS (
+            SELECT *
+            FROM DA.ISPIT I
+            WHERE I.INDEKS = D.INDEKS
+                AND I.IDPREDMETA = P.ID
+                AND I.STATUS NOT IN ('p', 'n')
+        )
+);
